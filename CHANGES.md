d0632101e591804d25bdb47b24bc74e11756f206
Michael Kirk <michael.code@endoftheworl.de>
2026-02-17 14:53:10 -0800

    pmtiles `show` command

---
+++ CHANGES.md
@@ -0,0 +1,4 @@
+## Unreleased
+
+- Add the `pmtiles show` command.
+  <https://github.com/michaelkirk/pmtiles-rs-cli/pull/2>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+//! CLI tool for interacting with a pmtiles archive built on top of the rust pmtiles crate.
+
+mod show;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "pmtiles")]
+#[command(about = "PMTiles CLI tool", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Inspect a local or remote archive
+    Show(show::Args),
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize logger from RUST_LOG environment variable
+    // Example: RUST_LOG=debug pmtiles show ...
+    env_logger::init();
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Show(args) => show::run(args).await,
+    }
 }
